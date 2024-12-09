2ec7b5dfdd6c9d6b8a3f0d1d3220846dbaa3109c
dunkeln <prateekpravanjan@gmail.com>
2024-12-09 11:58:31 -0500

    nascent stage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,74 +3,36 @@
   import { ModeWatcher } from "mode-watcher";
   import Sun from "lucide-svelte/icons/sun";
   import Moon from "lucide-svelte/icons/moon";
+  import { Video } from "lucide-svelte";
   import { toggleMode } from "mode-watcher";
   import Button from "$lib/components/ui/button/button.svelte";
-  import Menu from "$lib/components/Menu.svelte";
-
-  const asciiArt = `
-⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣤⣤⣤⣤⣤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⣠⣶⠟⠛⠉⠁⠀⠀⠀⠈⠉⠛⢷⣦⡀⠀⠀⠀⠀⠀
-⠀⠀⠀⣠⡾⠋⢁⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠉⢻⣦⡀⠀⠀⠀
-⠀⢀⣼⠟⠀⠀⠈⠙⠶⡄⠀⠀⠀⠚⢋⣀⣀⣀⣤⣄⠙⣿⡄⠀⠀
-⢀⣾⠋⣠⡶⢶⣶⣿⡟⢻⣦⠀⣾⣟⣻⣿⣿⡯⠿⠟⠀⠘⣿⡄⠀
-⣼⡏⠀⠙⠛⠛⠛⠛⠛⠛⠋⠀⠀⠉⠉⠁⠀⠀⠀⠀⠀⠀⠘⣿⡀
-⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣇
-⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿
-⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡟
-⢸⣧⠀⠀⠀⠀⠀⠀⠀⠀⠒⠛⠛⠃⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⠃
-⠀⢻⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡿⠃⠀
-⠀⠀⠙⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⡿⠋⠀⠀⠀
-⠀⠀⠀⠀⠙⠻⢦⣤⣤⣄⣀⣀⣤⣤⣤⣴⣶⠿⠛⠉⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠈⠉���⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
-         WHAT YOU WANT?
-  `;
-  const style = `
-    color: #AB4459; /* Bright red */
-    font-family: monospace;
-    font-size: 16px;
-    font-weight: bold;
-    background: none;
-    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
-    padding: 10px;
-  `;
-
-  console.log(`%c${asciiArt}`, style);
 </script>
 
-<!-- TODO: resolve keyboard events -->
-
-<div class="app p-[1rem] font-vazirmatn">
+<div
+  class="app p-[1rem] font-vazirmatn grid grid-cols-[6%_94%] h-[100%] border-2"
+>
   <ModeWatcher />
-  <header class="flex justify-between items-center gap-[1rem] pr-[1.3rem]">
-    <Button
-      target="_blank"
-      href="https://github.com/dunkeln/"
-      class="font-semibold p-[1.3rem]"
-      variant="link"
-    >
-      dunkeln.github.io
-    </Button>
-
-    <span class="inline">
-      <Menu />
-    </span>
+  <header
+    class="flex flex-col justify-between items-center gap-[1rem] border-* border-2 rounded-md h-full pt-[1.3rem] pb-[1.3rem] bg-secondary text-secondary-foreground"
+  >
+    <div class="grid grid-rows gap-[2rem]">
+      <Button variant="ghost">
+        <Video />
+      </Button>
+      <div>app2</div>
+    </div>
+    <div>
+      <Button on:click={toggleMode} variant="ghost" size="icon">
+        <Sun
+          class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        />
+        <Moon
+          class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        />
+      </Button>
+    </div>
   </header>
   <main class="p-[1.3rem] flex-grow justify-center">
     <slot></slot>
   </main>
-  <span class="pr-[1.3rem]">
-    <Button
-      on:click={toggleMode}
-      variant="outline"
-      size="icon"
-      class="fixed bottom-[1.3rem] right-[1.4rem]"
-    >
-      <Sun
-        class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-      />
-      <Moon
-        class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-      />
-    </Button>
-  </span>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 </script>
 
 <h1>template for web project</h1>
+
 <p>
   Visit <a href="https://github.com/dunkeln/www-skeleton/">site template</a> to get
   references
